b43538ba6093d7bce43ec6ab7ae15b138c7891c4
Jebson <jtfajilan@gmail.com>
2025-05-30 01:06:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 include "data.php";
 include "functions.php";
 
-// Group the movies by genre using a function
 $groupedMovies = groupByGenre($movies);
 
-// Display the grouped movies
 echo "<h1>Movie Catalog</h1>";
 displayMovies($groupedMovies);
 ?>
