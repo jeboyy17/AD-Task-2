03839a61961cdf04226669cdca00be7c39c1bf44
Jebson <jtfajilan@gmail.com>
2025-05-30 00:49:15 +0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,30 @@
+<?php
+// Array of movie dictionaries
+$movies = [
+    [
+        "title" => "The Matrix",
+        "genre" => "Sci-Fi",
+        "year" => 1999
+    ],
+    [
+        "title" => "Inception",
+        "genre" => "Sci-Fi",
+        "year" => 2010
+    ],
+    [
+        "title" => "The Godfather",
+        "genre" => "Crime",
+        "year" => 1972
+    ],
+    [
+        "title" => "Parasite",
+        "genre" => "Thriller",
+        "year" => 2019
+    ],
+    [
+        "title" => "Spirited Away",
+        "genre" => "Animation",
+        "year" => 2001
+    ]
+];
+?>
